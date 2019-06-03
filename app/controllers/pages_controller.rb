da319f8d114665906b9c0470f8d5e588f37fc3f4
maurabaptista <maura.mendes.baptista@gmail.com>
2019-06-03 12:07:58 +0100

    pictures resized & favicon inserted

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :accommodation]
+  skip_before_action :authenticate_user!, only: [:home, :accommodation, :calculator]
 
   def home
   	@rooms = Room.all
@@ -7,5 +7,35 @@ class PagesController < ApplicationController
 
   def accommodation
   	@rooms = Room.all
+  end
+
+  def calculator
+  	
+	  	@name1 = params[:name]
+	  	if @name1 == ""
+	  		@error_msg = "must enter your name!"
+	  	end
+	  	@email1 = params[:email]
+	  	@product1 = params[:product1]
+	  	@question = params[:question]
+	  	@newsleter = params["newslater"] == "newsleter"
+	  	gender = params[:gender]
+	  	@male = gender == "male"
+	  	@female = gender == "female"
+	  	# form tag variables
+	  	@name = params["Name"]
+  		@email  = params[:email]
+  		@age = params[:age]
+  		@products = %w(Skiis Poles Mittens)
+  		@selected_product = params[:product]
+  		@salad = params[:food] == "salad"
+  		@main = params[:food] == "main"
+  		@desert = params[:food] == "desert"
+  	
+  	
+  		
+
+
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'pages/accommodation'
+  # get 'pages/calculator'
+  # get 'calculator' => 'pages#calculator'
+  # post 'pages/calculator'
+
+  match "calculator" => "pages#calculator", :via => [:post, :get]
 
   resources :rooms, only: [:index, :show]
 
