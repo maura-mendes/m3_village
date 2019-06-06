594b23cf3bf55cea7d98d6074625bf88d4fbd368
maurabaptista <maura.mendes.baptista@gmail.com>
2019-06-07 00:07:32 +0100

    search results form and whole page finished

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,22 @@ class RoomsController < ApplicationController
   	skip_before_action :authenticate_user!, only: [:index, :show]
 
   	def index
-  		@rooms = Room.all
+
+  		# @rooms = Room.all
+  		@booking = Booking.new
+  		# pry-byebug
+  		@params_hash = request.query_parameters[:booking]
+   		 @bookings_params = @params_hash
+   		 @check_in_date = @bookings_params[:check_in_date]
+   		 @check_out_date = @bookings_params[:check_out_date]
+   		 @adult_guest = @bookings_params[:adult_guest]
+   		 @children_guest = @bookings_params[:children_guest]
+   		if params[:commit]
+   		    @rooms = Room.search(params[:search]).order("created_at DESC")
+   		else
+   		    @rooms = Room.all.order('created_at DESC')
+   		end
+
   	end
 
   	def show
@@ -11,3 +26,5 @@ class RoomsController < ApplicationController
 
 end
  
+
+
--- app/models/room.rb
+++ app/models/room.rb
@@ -4,9 +4,14 @@ class Room < ApplicationRecord
 	has_many :room_attachments
 	accepts_nested_attributes_for :room_attachments
 	# has_many :photos, through: :room_attachmens
+
+	def self.search(search)
+	  where(available: true) 
+	end
+
+	def photos
+	    RoomAttachment.where(room: self)
+	end
 end
 
 
-def photos
-    RoomAttachment.where(room: self)
-end
