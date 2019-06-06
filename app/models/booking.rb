53116fdf4ef3b638604440ebcacf8df668555be5
maurabaptista <maura.mendes.baptista@gmail.com>
2019-06-07 00:42:11 +0100

    search results form and whole page finishe

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :room
+  validates :check_in_date, :check_out_date, :adult_guest, :children_guest , presence: true
 
   ADULT_GUESTS = ["1 Adult", "2 Adults", "3 Adults", "4 Adults"]
   CHILDREN_GUESTS = ["0 Children", "1 Child", "2 Children", "3 Children", "4 Children"]
